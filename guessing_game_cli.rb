fe6ebfd3f077a6cbd812fd1458babeee2295c41b
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-28 20:09:59 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,10 +2,13 @@
 def run_guessing_game
   secret_number = rand(6)+1
   puts "Guess what number I picked: "
-  user_guess = gets.chomp.to_i
-  case user_guess
-  when secret_number
-    puts "You guessed the correct number!"
-  when !se
-  end
+  user_guess = gets.chomp
+  if user_guess === "exit" {
+    puts "Goodbye!"
+    return nil
+  } else {
+    if (user_guess === secret_number) {
+      puts ""
+    }
+  }
 end
