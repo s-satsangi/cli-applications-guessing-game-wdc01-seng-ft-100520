2552fb302f027c6c10c4e87f31df9071275fc26b
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-28 20:18:21 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   user_guess = gets.chomp
   if user_guess === "exit" 
     puts "Goodbye!"
-  elsif (user_guess === secret_number) 
+  elsif (user_guess.to_i === secret_number) 
       puts "You guessed the correct number!"
   else 
       puts "Sorry! The computer guessed #{secret_number}."
