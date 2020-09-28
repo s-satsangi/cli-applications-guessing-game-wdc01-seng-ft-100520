a640cf49deec1dcb4ac40580f381a1037f362be5
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-28 20:15:01 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,11 +6,12 @@ def run_guessing_game
   if user_guess === "exit" do
     puts "Goodbye!"
     return nil
-  else {
-    if (user_guess === secret_number) 
+  elsif (user_guess === secret_number) 
       puts "You guessed the correct number!"
-    else 
+  else 
       puts "Sorry! The computer guessed ${secret_number}"
-    end
-  }
+  end
+  
+  
+
 end
